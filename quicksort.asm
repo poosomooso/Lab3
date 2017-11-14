e036c8910a56e21360fd17182b301fe5991db4b1
Halliax <tmsheneman@gmail.com>
2017-11-13 20:28:02 -0500

    working quicksort

--- quicksort.asm
+++ quicksort.asm
@@ -1,20 +1,103 @@
 main:
-addi $t0, $zero, 3
-addi $t1, $zero, 2
-addi $t2, $zero, 7
-addi $t3, $zero, 9
-addi $t4, $zero, 5
+addi $t0, $zero, 9
+addi $t1, $zero, 5
+addi $t2, $zero, 3
+addi $t3, $zero, 6
+addi $t4, $zero, 2
+addi $t5, $zero, 8
+addi $t6, $zero, 7
+addi $t7, $zero, 3
+addi $t8, $zero, 1
+addi $t9, $zero, 4
 sw $t0, ($gp)
 sw $t1, 4($gp)
 sw $t2, 8($gp)
 sw $t3, 12($gp)
 sw $t4, 16($gp)
+sw $t5, 20($gp)
+sw $t6, 24($gp)
+sw $t7, 28($gp)
+sw $t8, 32($gp)
+sw $t9, 36($gp)
 add $s0, $zero, $gp
 addi $a0, $zero, 0
-addi $a1, $zero, 4
-jal partition
+addi $a1, $zero, 9
+jal quicksort
+j done
+
+
+quicksort:
+# $s0 = arr*
+# $s1 = pivot
+# $a0 = start
+# $a1 = end
+# $t3 = branch check temporary (for xori and slt)
+
+
+# if start < end, run quicksort
+slt $t3, $a0, $a1
+bne $t3, $zero, run
 j end
 
+run:
+# push frame onto stack
+addi $sp, $sp, -12
+sw $ra, 8($sp)
+sw $a0, 4($sp)
+sw $a1, ($sp)
+
+# pivot = partition (arr, start, end)
+jal partition
+
+# pop frame from stack
+lw $ra, 8($sp)
+lw $a0, 4($sp)
+lw $a1, ($sp)
+addi $sp, $sp, 12
+
+add $s1, $zero, $v0
+
+# push frame onto stack
+addi $sp, $sp, -16
+sw $ra, 12($sp)
+sw $a0, 8($sp)
+sw $a1, 4($sp)
+sw $s1, ($sp)
+
+# quicksort(arr, start, pivot - 1)
+addi $a1, $s1, -1
+
+jal quicksort
+
+# pop frame from stack
+lw $ra, 12($sp)
+lw $a0, 8($sp)
+lw $a1, 4($sp)
+lw $s1, ($sp)
+addi $sp, $sp, 16
+
+# push frame onto stack
+addi $sp, $sp, -12
+sw $ra, 8($sp)
+sw $a0, 4($sp)
+sw $a1, ($sp)
+
+# quicksort(arr, pivot + 1, end)
+add $a0, $s1, 1
+
+jal quicksort
+
+# pop frame from stack
+lw $ra, 8($sp)
+lw $a0, 4($sp)
+lw $a1, ($sp)
+addi $sp, $sp, 12
+
+end:
+jr $ra
+
+
+partition:
 # $v0 = return val
 # $s0 = arr*
 # $a0 = start
@@ -29,7 +112,6 @@ j end
 # $t6 = arr[i] val
 # $t7 = arr[j] val
 
-partition:
 addi $sp, $sp, -4
 sw $ra, ($sp)
 # -----------------------------------------------------------------
@@ -122,7 +204,7 @@ jal calcMemAddr
 # set reg arr[i] to mem[arr[i + 1]]
 lw $t6, ($t1)
 # store pivot at mem[arr[i + 1]]
-sw $t0 ($t1)
+sw $t0, ($t1)
 
 # set arr index to end and call calcMemAddr
 add $a2, $zero, $a1
@@ -152,4 +234,6 @@ bne $t3, $zero, pivotcalc
 jr $ra
 
 
-end:
+done:
+addi $v0, $zero, 10
+syscall
