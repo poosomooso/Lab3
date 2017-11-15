e317d55864cf8dd9dc5cbbd1252f224fc925d2ed
Bryan Werth <bryan.werth@students.olin.edu>
2017-11-14 21:00:08 -0500

    Added some things to the controller

--- CPUcontroller.v
+++ CPUcontroller.v
@@ -1,4 +1,4 @@
-module CPUcontroller(instruction, ALU0, ALU1, ALU2, ALU3, mux1, mux2, mux3, op, PCmux);
+module CPUcontroller(instruction, clk, ALU0, ALU1, ALU2, ALU3, mux1, mux2, mux3, op, PCmux);
 	input [31:0] instruction;
 	output [2:0] ALU0, ALU1, ALU2, ALU3;
 	output mux1, op;
@@ -8,6 +8,8 @@ module CPUcontroller(instruction, ALU0, ALU1, ALU2, ALU3, mux1, mux2, mux3, op, 
 	wire [15:0] immediate;
 	wire [5:0] opcode, funct;
 	wire [4:0] rs, rt, rd, shamt;
+	reg [3:0] action_type;
+	reg [5:0] counter;
 	
 	assign opcode = instruction[31:26];
 	assign rs = instruction[25:21];
@@ -18,3 +20,41 @@ module CPUcontroller(instruction, ALU0, ALU1, ALU2, ALU3, mux1, mux2, mux3, op, 
 	assign immediate = instruction[15:0];
 	assign address = instruction[25:0];
 	
+	always @ (posedge clk)
+		if (counter == 0 && {Conditions for J}) begin
+			counter <= 1;
+			action_type <= 4'b0001;
+		end else if (counter == 0 && {Conditions for BNE}) begin
+			counter <= 1;
+			action_type <= 4'b0010;
+		end else if (counter == 0 && {Conditions for JAL}) begin
+			counter <= 1;
+			action_type <= 4'b0011;
+		end else if (counter == 0 && {Conditions for Add}) begin
+			counter <= 1;
+			action_type <= 4'b0100;
+		end else if (counter == 0 && {Conditions for Addi}) begin
+			counter <= 1;
+			action_type <= 4'b0101;
+		end else if (counter == 0 && {Conditions for Xori}) begin
+			counter <= 1;
+			action_type <= 4'b0110;
+		end else if (counter == 0 && {Conditions for LW}) begin
+			counter <= 1;
+			action_type <= 4'b0111;
+		end else if (counter == 0 && {Conditions for SW}) begin
+			counter <= 1;
+			action_type <= 4'b1000;
+		end else if (counter == 0 && {Conditions for JR}) begin
+			counter <= 1;
+			action_type <= 4'b1001;
+		end else if (counter == 0 && {Conditions for Sub}) begin
+			counter <= 1;
+			action_type <= 4'b1010;
+		end else if (counter == 0 && {Conditions for SLT}) begin
+			counter <= 1;
+			action_type <= 4'b1011;
+		end else if (counter == {ceiling}) begin
+			counter <= 0;
+			action_type <= 0;
+		
