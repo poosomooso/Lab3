b469bf2f040221023eacf78f0b15cb1ca7934751
Serena Chen <serenazychen@gmail.com>
2017-11-17 09:28:42 -0500

    added a makefile for better compiling

---
+++ makefile
@@ -0,0 +1,8 @@
+cpu: cpu.t.v
+	iverilog -o cpu.t.o cpu.t.v
+
+test: alu.t.v instructiondecoder.t.v regfile.t.v signextend.t.v
+	iverilog -o alu.t.o alu.t.v
+	iverilog -o instructiondecoder.t.o instructiondecoder.t.v
+	iverilog -o regfile.t.o regfile.t.v
+	iverilog -o signextend.t.o signextend.t.v
--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 if [ $1 == "test" ]; then
-	iverilog -o alu.t.o alu.t.v
+	make test
 	./alu.t.o
-	iverilog -o instructiondecoder.t.o instructiondecoder.t.v
 	./instructiondecoder.t.o
-	iverilog -o regfile.t.o regfile.t.v
 	./regfile.t.o
-	iverilog -o signextend.t.o signextend.t.v
 	./signextend.t.o
 elif [ $1 == "cpu" ]; then
-	iverilog -o cpu.t.o cpu.t.v
+	make cpu
 	./cpu.t.o
 fi
