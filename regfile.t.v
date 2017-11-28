11a7f0736fac4331169b86b6be2253d72296b554
Serena Chen <serenazychen@gmail.com>
2017-11-27 22:50:30 -0500

    actually fixed the test

--- regfile.t.v
+++ regfile.t.v
@@ -169,16 +169,14 @@ output reg		Clk
       #5 Clk=1; #5 Clk=0;
 
       for (j = 0; j <= 'b11111; j = j + 2) begin
-        if (j != i) begin
-          RegWrite = 0;
-          ReadRegister1 = j;
-          ReadRegister2 = j + 1;
-          #5 Clk=1; #5 Clk=0;
+        RegWrite = 0;
+        ReadRegister1 = j;
+        ReadRegister2 = j + 1;
+        #5 Clk=1; #5 Clk=0;
 
-          if((ReadData1 == i) || (ReadData2 == i)) begin
-            dutpassed = 0;
-            $display("Test Case 4 Failed : value for register %d written to multiple registers", i);
-          end
+        if((j != i && ReadData1 == WriteData) || (j+1 != i && ReadData2 == WriteData)) begin
+          dutpassed = 0;
+          $display("Test Case 4 Failed : value for register %d written to multiple registers", i);
         end
       end
     end
