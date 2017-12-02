df84abd87d0c0854ac133af6b54df91dcfe9e1e5
Bryan Werth <bryan.werth@students.olin.edu>
2017-12-01 22:03:18 -0500

    Add files via upload

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,32 +1,23 @@
-//`include "instruction_memory.v"
+module instruction_memory
+(
+	input clk, regWE,
+	input[31:0] Addr,
+	input[31:0] DataIn,
+	output[31:0] DataOut
+);
 
-module instruction_memory_test ();
-
-  // Instantiate device/module under test
-  reg clk,regWE;
-  reg [31:0] DataIn,Addr;  // Primary test inputs
-  wire [31:0] DataOut;    // Test outputs
-
-  initial clk=0;
-  always #10 clk=!clk;
-  
-  instruction_memory im(.clk (clk),.regWE (regWE),.DataIn (DataIn),.Addr (Addr),.DataOut (DataOut));  // Module to be tested
-  
-  task checkTestCase;
-  input [31:0] expectedOut;
-  begin
-	if (DataOut != expectedOut) begin
-		$display("Test Case Failed: expected %b, received %b",expectedOut,DataOut);
-	end
-  end
-  endtask
+	reg [31:0] mem[1023:0];
+	
+	always@(posedge clk) begin
+		if (regWE) begin
+			mem[Addr] <= DataIn;
+		end
 		
-
-  // Run sequence of test stimuli
-  initial begin
-    regWE = 1'b1; DataIn = 20; Addr = 20; #20;                                // Set A and B, wait for update (#1)
-    checkTestCase(20); #20;
-    regWE = 0; DataIn = 40; Addr = 20; #20;  
-    checkTestCase(20); #20;
-  end
-endmodule    // End demorgan_test
+	end
+	
+	initial begin
+		$readmemh("mem.dat",mem);
+	end
+	
+		assign DataOut = mem[Addr>>2];
+endmodule
